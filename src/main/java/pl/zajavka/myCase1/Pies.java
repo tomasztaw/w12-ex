dd886abac93c1224149919c6e9492fe9495e3e68
Tomasz Aleksander <dopracyzgitem@jakismail.pl>
2023-04-03 19:49:34 +0200

    Revert "Mod-2"
    This reverts commit 07c8e5eb

--- src/main/java/pl/zajavka/myCase1/Pies.java
+++ src/main/java/pl/zajavka/myCase1/Pies.java
@@ -2,8 +2,4 @@ package pl.zajavka.myCase1;
 
 public class Pies extends Zwierzak {
     private String name;
-
-    public void sniff() {
-        System.out.println("I'm soo sniffing!");
-    }
 }
--- src/main/java/pl/zajavka/myCase1/Zwierzak.java
+++ src/main/java/pl/zajavka/myCase1/Zwierzak.java
@@ -2,8 +2,6 @@ package pl.zajavka.myCase1;
 
 public abstract class Zwierzak {
 
-    private String name;
-
     protected String whatIsYourName() {
         return "I don't have any";
     }
